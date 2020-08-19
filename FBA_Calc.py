f2f234de1dfe55d9db05dc24e708b4893f11a804
Evan Archuleta <Evan.archuleta02@gmail.com>
2020-08-18 19:24:57 -0600

    Created dictionaries for product tier sizes

--- FBA_Calc.py
+++ FBA_Calc.py
@@ -1,18 +1,25 @@
-"""
-Helpful Amazon Links (sign on to Seller Central required)
-
-Calculate FBA Fees From Weight and Dimensions
-https://sell.amazon.com/pricing.html?ref_=asus_soa_rd&#fulfillment-fees
-Dimensional Weight and more info
-https://sellercentral.amazon.com/gp/help/help.html?itemID=G201112670&language=en_US#:~:text=The%20dimensional%20weight%20is%20equal,up%20to%20the%20nearest%20pound.
-Product Size Tiers 
-https://sellercentral.amazon.com/gp/help/G201105770?language=en_US&ref=ag_G201105770_cont_G201112670
-Use this to test 
-https://sellercentral.amazon.com/fba/revenuecalculator/index?lang=en_US
-"""
-
 import pandas as pd
 import numpy as np
 
 df = pd.read_csv("sku_info.csv")
 
+# add 4 oz for packaging weight in this size tier
+# greater of dim weight or unit weight + packaging then roundup to whole pound
+standard_size_product_tiers = {
+    "Small standard-size (10 oz or less)": 2.50,
+    "Small standard-size (10+ to 16 oz)" : 2.63,
+    "Large standard-size (10 oz or less)": 3.31,
+    "Large standard-size (10+ to 16 oz)": 3.48,
+    "Large standard-size (1+ to 2 lb)": 4.90,
+    "Large standard-size (2+ to 3 lb)": 5.42,
+    "Large standard-size (3+ lb to 21 lb)": "$5.42 + $0.38/lb above first 3 lb"
+}
+
+# add 1 lb for packaging weight in this size tier
+# greater of dim weight or unit weight + packaging then roundup to whole pound
+oversized_product_tiers = {
+    " Small oversize 71 lb or less" : "$8.26 + $0.38/lb above first 2 lb",
+    " Medium oversize 151 lb or less": "$11.37 + $0.39/lb above first 2 lb",
+    " Large oversize 151 lb or less": "$75.78 + $0.79/lb above first 90 lb",
+    " Special oversize" : "$137.32 + $0.91/lb above first 90 lb"
+}
--- README.txt
+++ README.txt
@@ -1,5 +1,18 @@
-This project is to calculate Amazon FBA fulfilment fees given the items weight and dimensions. This calculator is for non-apparel items at this time, but a future version may include an apparel calculator. 
+This project is to calculate Amazon FBA fulfilment fees given the items weight and dimensions. This calculator is for non-apparel items at this time, but a future version may include an apparel calculator. This calculator also is not for hazardous goods. Both hazardous goods and apparel are charged differently. A future version might incorporate these. 
 
-The CSV loaded in should have five columns: sku, longest-side, median-side, shortest-side, item-package-weight. The units should be in lbs and inches. 
+The CSV loaded in should have five columns: sku, longest-side, median-side, shortest-side, item-package-weight. The units should be in lbs and inches. This file should be named "sku_info.csv". 
  
+Helpful Amazon links (sign on to Seller Central required)
 
+Calculate FBA Fees From Weight and Dimensions
+https://sell.amazon.com/pricing.html?ref_=asus_soa_rd&#fulfillment-fees
+
+Dimensional Weight and more info
+https://sellercentral.amazon.com/gp/help/help.html?itemID=G201112670&language=en_US#:~:text=The%20dimensional%20weight%20is%20equal,up%20to%20the%20nearest%20pound.
+
+Product Size Tiers 
+https://sellercentral.amazon.com/gp/help/G201105770?language=en_US&ref=ag_G201105770_cont_G201112670
+
+Use this to test 
+https://sellercentral.amazon.com/fba/revenuecalculator/index?lang=en_US
+"""
