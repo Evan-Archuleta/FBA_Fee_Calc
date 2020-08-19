20028e5e66a3598ca36bde443f392c259b9a9a0b
Evan Archuleta <Evan.archuleta02@gmail.com>
2020-08-18 18:59:20 -0600

    Added README file with instructions

--- FBA_Calc.py
+++ FBA_Calc.py
@@ -1,11 +1,15 @@
 """
+Helpful Amazon Links (sign on to Seller Central required)
+
 Calculate FBA Fees From Weight and Dimensions
 https://sell.amazon.com/pricing.html?ref_=asus_soa_rd&#fulfillment-fees
 Dimensional Weight and more info
 https://sellercentral.amazon.com/gp/help/help.html?itemID=G201112670&language=en_US#:~:text=The%20dimensional%20weight%20is%20equal,up%20to%20the%20nearest%20pound.
+Product Size Tiers 
+https://sellercentral.amazon.com/gp/help/G201105770?language=en_US&ref=ag_G201105770_cont_G201112670
+Use this to test 
+https://sellercentral.amazon.com/fba/revenuecalculator/index?lang=en_US
 """
-# Use this to test 
-# https://sellercentral.amazon.com/fba/revenuecalculator/index?lang=en_US
 
 import pandas as pd
 import numpy as np
---
+++ README.txt
@@ -0,0 +1,5 @@
+This project is to calculate Amazon FBA fulfilment fees given the items weight and dimensions. This calculator is for non-apparel items at this time, but a future version may include an apparel calculator. 
+
+The CSV loaded in should have five columns: sku, longest-side, median-side, shortest-side, item-package-weight. The units should be in lbs and inches. 
+ 
+
